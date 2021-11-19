d5b4f0ea87aa6a2e763acf6dee4bd9b8d7e671d3
Nguyễn Viết Ca <nguyenvietca199x@gmail.com>
2021-11-19 10:03:17 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,14 +1,22 @@
+/**
+ * nvca
+ * */
 var noel = "#txtNoel";
 var dl = "#txtDl";
 var al = "#txtAl";
 
+var currentYear = new Date().getFullYear();
 var dateNoel = "12/25/2021";
+// dateNoel = "2021/12/25";
+//var dateDl = "01/01/" + ++currentYear;
 var dateDl = "01/01/2022";
 CountDownTimer(dateNoel, noel);
 CountDownTimer(dateDl, dl);
 CountDownTimer("02/01/2022", al);
 
 setInterval(() => {
+    currentDate = new Date().toLocaleString();
+    $("#currentDate").html(currentDate);
     randomColors();
 }, 1000);
 
@@ -24,33 +32,55 @@ function CountDownTimer(dt, id) {
         var now = new Date().getTime();
         var distance = end - now;
         if (distance < 0) {
-            var noel = "#txtNoel";
-            var dl = "#txtDl";
-            var al = "#txtAl";
             clearInterval(timer);
             var welcome = "";
             switch (id) {
                 case noel:
                     welcome = "chúc mừng giáng sinh";
+                    $("#days").html("CHÚC");
+                    document.getElementById("days").className = "display-2";
+                    $("#hours").html("MỪNG");
+                    document.getElementById("hours").className = "display-3";
+                    $("#minutes").html("GIÁNG");
+                    document.getElementById("minutes").className = "display-3";
+                    $("#seconds").html("SINH");
+                    document.getElementById("seconds").className = "display-2";
+                    // document.getElementById("snow").className = "hidden-snow";
                     break;
                 case dl:
                     welcome = "chúc mừng năm mới";
+                    $("#dldays").html("CHÚC");
+                    document.getElementById("dldays").className = "display-2";
+                    $("#dlhours").html("MỪNG");
+                    document.getElementById("dlhours").className = "display-3";
+                    $("#dlminutes").html("NĂM");
+                    document.getElementById("dlminutes").className = "display-3";
+                    $("#dlseconds").html("MỚI");
+                    document.getElementById("dlseconds").className = "display-2";
                     break;
                 case al:
                     welcome = "chúc mừng năm mới sức khỏe và an khang";
+                    $("#aldays").html("CHÚC");
+                    document.getElementById("aldays").className = "display-2";
+                    $("#alhours").html("MỪNG");
+                    document.getElementById("alhours").className = "display-3";
+                    $("#alminutes").html("NĂM");
+                    document.getElementById("alminutes").className = "display-3";
+                    $("#alseconds").html("MỚI");
+                    document.getElementById("alseconds").className = "display-2";
                     break;
 
                 default:
                     break;
             }
-            $(id).html("<h2>" + welcome + " < /h2>");
+
             return;
         }
         var days = Math.floor(distance / _day);
         var hours = Math.floor((distance % _day) / _hour);
         var minutes = Math.floor((distance % _hour) / _minute);
         var seconds = Math.floor((distance % _minute) / _second);
-        // var datacount = days + 'ngày ' + hours + 'giờ ' + minutes + 'phút ' + seconds + 'giây';
+        var datacount = days + 'ngày ' + hours + 'giờ ' + minutes + 'phút ' + seconds + 'giây';
         // $(id).html(datacount);
 
         switch (id) {
@@ -72,6 +102,7 @@ function CountDownTimer(dt, id) {
                 $("#alminutes").html(minutes);
                 $("#alseconds").html(seconds);
                 break;
+
             default:
                 break;
         }
@@ -82,11 +113,13 @@ function CountDownTimer(dt, id) {
     timer = setInterval(showRemaining, 1000);
 }
 
-
 function randomColors() {
     var colors = ["Green", "Yellow", "Orange", "Pink", "Gray", "Red", "Brown", "Violet", "#FF1493", "#FFB6C1", "#D8BFD8", "#FF00FF", "#8A2BE2", "#FFA07A", "#FA8072", "#F08080", "#DC143C", "#FF0000", "#FF8C00", "#FF4500", "#FFD700", "#BDB76B", "#ADFF2F", "#ADFF2F", "#9ACD32", "#8FBC8F", "#008B8B"];
     var index = Math.floor(Math.random() * colors.length);
     document.getElementById("hello").style.color = colors[index];
+
+    var indexCurrentDate = Math.floor(Math.random() * colors.length);
+    document.getElementById("currentDate").style.color = colors[indexCurrentDate];
     var cardTxt = document.getElementsByClassName("nav-link");
     for (var i = 0; i < cardTxt.length; i++) {
         cardTxt[i].style.color = colors[index]
